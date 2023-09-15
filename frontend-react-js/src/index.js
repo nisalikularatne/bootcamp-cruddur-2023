b8f80aeb2ee27ce1cd7ad37a70804ff72b1f6168
Nisali Sanara Kularatne <nisalikularatne@gmail.com>
2023-09-15 15:06:44 +0000

    Honeycomb stuff

--- frontend-react-js/src/index.js
+++ frontend-react-js/src/index.js
@@ -4,6 +4,42 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { WebTracerProvider, BatchSpanProcessor } from '@opentelemetry/sdk-trace-web';
+import { ZoneContextManager } from '@opentelemetry/context-zone';
+import { Resource }  from '@opentelemetry/resources';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
+
+// The exporter is responsible for sending traces from the browser to your collector
+const exporter = new OTLPTraceExporter({
+  url: "https://api.honeycomb.io",
+  headers: {
+    "x-honeycomb-team": "Oqrmp5I3ooG0dzuvTiNxfB",
+  },
+});
+console.log('env front end',process.env)
+// The TracerProvider is the core library for creating traces
+const provider = new WebTracerProvider({
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'browser',
+  }),
+});
+// The processor sorts through data as it comes in, before it is sent to the exporter
+provider.addSpanProcessor(new BatchSpanProcessor(exporter));
+// A context manager allows OTel to keep the context of function calls across async functions
+// ensuring you don't have disconnected traces
+provider.register({
+  contextManager: new ZoneContextManager()
+});
+
+// 
+registerInstrumentations({
+  instrumentations: [
+    getWebAutoInstrumentations(),
+  ],
+});
 const el_main = document.getElementsByTagName('main')[0];
 const root = ReactDOM.createRoot(el_main);
 root.render(
